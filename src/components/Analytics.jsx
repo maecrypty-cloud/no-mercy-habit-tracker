627c4096dcf9620a5e54bfe0a5d6ac2292cb65b9
maecrypty-cloud <maecrypty@gmail.com>
2025-07-23 18:34:35 +0530

    Create Analytics.jsx

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,8 @@
+export default function Analytics() {
+  return (
+    <div className="p-3 mt-4 bg-gray-100 rounded">
+      <h2 className="text-xl font-bold">Analytics</h2>
+      <p>Coming soon...</p>
+    </div>
+  )
+}
