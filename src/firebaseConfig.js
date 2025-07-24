5caddf9724cddfab4b7c6140199798dd77c9e600
maecrypty-cloud <maecrypty@gmail.com>
2025-07-24 08:58:57 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,20 @@
-// firebaseConfig.js (CDN Version)
+// firebaseConfig.js (CDN version)
+
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDrqLVyX7d3_fb9CPjcvs3srh8hRA7d1OI",
   authDomain: "no-mercy-2e372.firebaseapp.com",
   projectId: "no-mercy-2e372",
-  storageBucket: "no-mercy-2e372.firebasestorage.app",
+  storageBucket: "no-mercy-2e372.appspot.com", // <- ye correct karo (.app hatao)
   messagingSenderId: "363291533348",
   appId: "1:363291533348:web:2deec586a5bc831b9678b1",
   measurementId: "G-XD74PL8KXJ"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Firebase services
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 const db = firebase.firestore();
-
-export { auth, provider, db };
