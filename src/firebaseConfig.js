d1244f243f2d87ce1fe9b3536c3ef6d10bc44d5e
maecrypty-cloud <maecrypty@gmail.com>
2025-07-24 08:24:39 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,18 @@
-// src/firebaseConfig.js
-
-// Firebase CDN se load ho chuka hai isliye window.firebase use karenge
+// firebaseConfig.js (CDN version)
 const firebaseConfig = {
   apiKey: "AIzaSyDrqLVyX7d3_fb9CPjcvs3srh8hRA7d1OI",
   authDomain: "no-mercy-2e372.firebaseapp.com",
   projectId: "no-mercy-2e372",
-  storageBucket: "no-mercy-2e372.appspot.com",
+  storageBucket: "no-mercy-2e372.firebasestorage.app",
   messagingSenderId: "363291533348",
   appId: "1:363291533348:web:2deec586a5bc831b9678b1",
   measurementId: "G-XD74PL8KXJ"
 };
 
+// Firebase initialize
 firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const db = firebase.firestore();
 
-export const auth = firebase.auth();
-export const provider = new firebase.auth.GoogleAuthProvider();
-export const db = firebase.firestore();
+export { auth, provider, db };
