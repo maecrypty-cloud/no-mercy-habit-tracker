250eadc2605b214a3cf7d8f6b55c4365818ca7da
maecrypty-cloud <maecrypty@gmail.com>
2025-07-24 11:10:19 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,11 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyB3GgAgQvcuWElNsrZ0FaZSSYoPY0tnSTw",
-  authDomain: "no-mercy-28e0a.firebaseapp.com",
+  authDomain: "no-mercy-28e0a.firebaseapp.com", // ✅ correct
   projectId: "no-mercy-28e0a",
-  storageBucket: "no-mercy-28e0a.firebasestorage.app",
+  storageBucket: "no-mercy-28e0a.appspot.com", // ✅ correct
   messagingSenderId: "353208485106",
   appId: "1:353208485106:web:bc33f4d201cbfd95f8fc6b",
   measurementId: "G-DT0SXRFFGR"
